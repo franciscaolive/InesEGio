ecfed6c53736e7be3971b94aabdebc253aaa54e7
Francisca Miranda <franciscaolivmir@gmail.com>
2026-02-06 02:40:49 +0000

    added audios

--- js/telefonemas.js
+++ js/telefonemas.js
@@ -14,6 +14,14 @@ if (callRows.length) {
     }
   };
 
+  const setActiveSection = (section) => {
+    clearActive();
+    if (section) {
+      section.classList.add('playing');
+      activeSection = section;
+    }
+  };
+
   const playCall = (row) => {
     const section = row.closest('.call-section');
     const audioSrc = row.dataset.audio;
@@ -23,29 +31,39 @@ if (callRows.length) {
       return;
     }
 
-    if (activeSrc !== audioSrc) {
-      audioPlayer.src = audioSrc;
-      activeSrc = audioSrc;
+    const isSameTrack = activeSrc === audioSrc;
+    const isPlaying = !audioPlayer.paused;
+
+    if (isSameTrack && isPlaying) {
+      audioPlayer.pause();
+      clearActive();
+      return;
     }
 
-    clearActive();
-    if (section) {
-      section.classList.add('playing');
-      activeSection = section;
+    if (isSameTrack && !isPlaying) {
+      setActiveSection(section);
+      audioPlayer.play().catch((err) => {
+        console.error('Unable to resume audio', err);
+        clearActive();
+      });
+      return;
     }
 
+    audioPlayer.pause();
+    audioPlayer.src = audioSrc;
+    activeSrc = audioSrc;
     audioPlayer.currentTime = 0;
+    setActiveSection(section);
     audioPlayer.play().catch((err) => {
       console.error('Unable to play audio', err);
       clearActive();
+      activeSrc = '';
     });
   };
 
-  audioPlayer.addEventListener('ended', clearActive);
-  audioPlayer.addEventListener('pause', () => {
-    if (audioPlayer.currentTime === 0) {
-      clearActive();
-    }
+  audioPlayer.addEventListener('ended', () => {
+    clearActive();
+    activeSrc = '';
   });
 
   const bindClick = (selector) => {
